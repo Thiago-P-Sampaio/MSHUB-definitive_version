7d8f91bd62b01a778e0374cc66086ae101e86781
Thiago-P-Sampaio <thiago.p.sampaio07@gmail.com>
2024-11-14 18:35:13 -0300

    Save file in CLoud + get URL + Save URL in BD

--- src/main/java/com/studenthub/api/controller/ControllerImage.java
+++ src/main/java/com/studenthub/api/controller/ControllerImage.java
@@ -5,7 +5,6 @@ import com.studenthub.api.domain.Aluno;
 import com.studenthub.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("image")
@@ -26,12 +24,18 @@ public class ControllerImage {
     AlunoRepository repository;
 
 
+
+
     @PostMapping("/upload")
     public ResponseEntity<Map> uploadImage(@RequestParam("file") MultipartFile file) {
         try {
             Map uploadResult = imageService.uploadImage(file);
             String imageUrl = (String) uploadResult.get("url"); // Obtendo a URL da imagem
-            Aluno aluno = repository.findLastUser();
+            Aluno exists = repository.findLastStudent();
+            exists.getClass();
+            exists.setImagURL(imageUrl);
+            repository.save(exists);
+
             return ResponseEntity.ok(Map.of("url", imageUrl)); // Retornando a URL em um mapa
         } catch (Exception e) {
             return ResponseEntity.status(500).body(Map.of("error", "Upload failed: " + e.getMessage()));
--- src/main/java/com/studenthub/api/repository/AlunoRepository.java
+++ src/main/java/com/studenthub/api/repository/AlunoRepository.java
@@ -1,16 +1,19 @@
 package com.studenthub.api.repository;
 
 import com.studenthub.api.domain.Aluno;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 @Repository
-@EnableJpaRepositories
 public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
 
-    @Query("SELECT a FROM Aluno a ORDER BY a.id DESC  LIMIT 1")
-    Aluno findLastUser();
+    @Query(value = "SELECT * FROM aluno ORDER BY id DESC LIMIT 1", nativeQuery = true)
+    Aluno findLastStudent();
 }
