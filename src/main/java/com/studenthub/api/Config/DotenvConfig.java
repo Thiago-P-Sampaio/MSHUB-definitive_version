202fcd8a359969f6b258aac28282d80b64942ce3
Thiago-P-Sampaio <thiago.p.sampaio07@gmail.com>
2024-11-19 22:36:29 -0300

    Remove dotenv library

--- src/main/java/com/studenthub/api/Config/DotenvConfig.java
+++ src/main/java/com/studenthub/api/Config/DotenvConfig.java
@@ -5,11 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class DotenvConfig {
 
-    @Bean
-    public Dotenv dotenv() {
-        return Dotenv.load();
-    }
 }
